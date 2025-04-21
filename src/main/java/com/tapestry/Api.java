677410eca8276a56e8109254c43c02a974e7a602
Ahmed <akatary01@gmail.com>
2025-04-21 07:21:49 +0000

    chaning app variable to api

--- src/main/java/com/tapestry/Api.java
+++ src/main/java/com/tapestry/Api.java
@@ -4,7 +4,7 @@ import io.javalin.Javalin;
 
 public class Api {
     public static void main(String[] args) {
-        var app = Javalin.create(/*config*/)
+        var api = Javalin.create(/*config*/)
             .get("/", ctx -> ctx.result("Hello World"))
             .start(7070);
     }
