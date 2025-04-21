88eaedc697c8ed9e586a11b8637c880089aa6481
Ahmed <atkatary@gmail.com>
2025-04-21 07:15:48 +0000

    initial api setup with javalin

---
+++ src/main/java/com/tapestry/Api.java
@@ -0,0 +1,11 @@
+package com.tapestry;
+
+import io.javalin.Javalin;
+
+public class Api {
+    public static void main(String[] args) {
+        var app = Javalin.create(/*config*/)
+            .get("/", ctx -> ctx.result("Hello World"))
+            .start(7070);
+    }
+}
